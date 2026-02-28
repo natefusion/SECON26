34006bf2038c23fe80f37679693087961ac985be
Nathan Piel <nthnpiel@gmail.com>
2026-02-27 21:03:21 -0500

    Flight Controller has Real(tm) C code now

--- flight-team/uav/main/Flight_Controller/flight_controller.c
+++ flight-team/uav/main/Flight_Controller/flight_controller.c
@@ -10,7 +10,10 @@
 
 #define MIN_THROTTLE 0.06f
 
-struct {
+// arbitrary
+#define GYRO_ID 55
+
+struct Pid {
     float kp;
     float ki;
     float kd;
@@ -20,24 +23,25 @@ struct {
 
     float setpoint;
     float integral_limit;
-} Pid;
+};
 
 enum Pid_Type {
     Pitch, Yaw, Roll, Height, Num_Pid_Types
-}
+};
 
 static float _throttle = 0.0f;
+static bool _should_run = true;
 
-static motor_config cfg[4] = {
+static motor_config _cfg[4] = {
     [0] = {.pin = 43, .task_name="M1"},
     [1] = {.pin =  8, .task_name="M2"},
     [2] = {.pin =  1, .task_name="M3"},
     [3] = {.pin =  9, .task_name="M4"},
 };
 
-static motor_handle *mh[4] = {0};
+static motor_handler *_mh[4] = {0};
 
-static pid[Num_Pid_Types] = {
+static struct Pid _pid[Num_Pid_Types] = {
     [Pitch] = (struct Pid){
     },
     [Roll] = (struct Pid){
@@ -48,9 +52,9 @@ static pid[Num_Pid_Types] = {
     },
 };
 
-static inline float clamp(float *x, float y) {
-    if (x < -y) *x = -y;
-    if (x > y) *x = y;
+static inline void clamp(float *x, float y) {
+    if (*x < -y) *x = -y;
+    if (*x > y) *x = y;
 }
 
 static float update_pid_angle(struct Pid *p, float angle, float rate) {
@@ -59,14 +63,14 @@ static float update_pid_angle(struct Pid *p, float angle, float rate) {
     if (_throttle <= MIN_THROTTLE) {
         p->integral = 0.0f;
     } else {
-        p->integral += error * DT;
+        p->integral += err * DT;
         clamp(&p->integral, p->integral_limit);
     }
 
     float derivative = rate;
 
     float output = p->kp*err + p->ki*p->integral - p->kd*derivative;
-    clamp(&output, p->output_limit);
+    clamp(&output, 1.0f);
 
     return output;
 }
@@ -77,13 +81,13 @@ static float update_pid_rate(struct Pid *p, float rate) {
     if (_throttle <= MIN_THROTTLE) {
         p->integral = 0.0f;
     } else {
-        p->integral += error * DT;
+        p->integral += err * DT;
         clamp(&p->integral, p->integral_limit);
     }
 
-    float derivative = (err - p->prev_error) / DT;
+    float derivative = (err - p->prev_err) / DT;
     float output = p->kp*err + p->ki*p->integral - p->kd*derivative;
-    clamp(&output, p->output_limit);
+    clamp(&output, 1.0f);
 
     p->prev_err = err;
 
@@ -91,15 +95,15 @@ static float update_pid_rate(struct Pid *p, float rate) {
 }
 
 void set_pitch(float rad) {
-    pid[Pitch].setpoint = rad;
+    _pid[Pitch].setpoint = rad;
 }
 
 void set_roll(float rad) {
-    pid[Roll].setpoint = rad;
+    _pid[Roll].setpoint = rad;
 }
 
 void set_yaw(float rad) {
-    pid[Yaw].setpoint = rad;
+    _pid[Yaw].setpoint = rad;
 }
 
 void set_throttle(float t) {
@@ -107,34 +111,42 @@ void set_throttle(float t) {
 }
 
 static void flight_task(void*) {
-    while (true) {
+    while (_should_run) {
         sensors_event_t ev;
         bno055_getEvent1(&ev);
         // ensure everything is in radians pls
         float ax = ev.orientation.x;
         float ay = ev.orientation.y;
-        float az = ev.orientation.z;
         float rx = ev.gyro.x;
         float ry = ev.gyro.y;
         float rz = ev.gyro.z;
         
-        float p = update_pid_angle(&p[Pitch], ax, rx);
-        float r = update_pid_angle(&p[Roll], ay, ry);
-        float y = update_pid_rate(&p[Yaw], az, rz);
+        float p = update_pid_angle(&_pid[Pitch], ax, rx);
+        float r = update_pid_angle(&_pid[Roll], ay, ry);
+        float y = update_pid_rate(&_pid[Yaw], rz);
         float t = _throttle;
 
         // ensure y signs are correct
-        set_motor_speed_pcnt(mh[0], t + p + r + y); // front right
-        set_motor_speed_pcnt(mh[1], t + p - r - y); // front left
-        set_motor_speed_pcnt(mh[2], t - p + r + y); // back right
-        set_motor_speed_pcnt(mh[3], t - p - r - y); // back left
+        set_motor_speed_pcnt(_mh[0], t + p + r + y); // front right
+        set_motor_speed_pcnt(_mh[1], t + p - r - y); // front left
+        set_motor_speed_pcnt(_mh[2], t - p + r + y); // back right
+        set_motor_speed_pcnt(_mh[3], t - p - r - y); // back left
 
         vTaskDelay(SLEEP_TIME / portTICK_PERIOD_MS);    
     }
+
+    set_motor_speed_pcnt(_mh[0], 0); // front right
+    set_motor_speed_pcnt(_mh[1], 0); // front left
+    set_motor_speed_pcnt(_mh[2], 0); // back right
+    set_motor_speed_pcnt(_mh[3], 0); // back left
 }
 
-bool flight_controller_start(void) {
-    book ok = bno055_begin(GYRO_ID, OPERATION_MODE_NDOF, BNO055_ADDRESS_A);
+void emergency_stop(void) {
+    _should_run = false;
+}
+
+bool flight_controller_init(void) {
+    bool ok = bno055_begin(GYRO_ID, OPERATION_MODE_NDOF, BNO055_ADDRESS_A);
     if (!ok) {
         printf("Gyro not found");
         return false;
@@ -142,7 +154,7 @@ bool flight_controller_start(void) {
 
     bno055_setExtCrystalUse(true);
 
-    for (int i = 0; i < 4; ++i) mh[i] = init_motor(&cfg[i]);
+    for (int i = 0; i < 4; ++i) _mh[i] = init_motor(&_cfg[i]);
 
     xTaskCreate(flight_task, "flight_controller", 4096, NULL, 0, NULL);
 
---
+++ flight-team/uav/main/Flight_Controller/flight_controller.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdbool.h>
+
+bool flight_controller_init(void);
+
+void emergency_stop(void);
